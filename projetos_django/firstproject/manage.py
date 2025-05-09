8b1d14142aa960c8f685609086b482d36a2e242f
Glailtonnascimento <glailtonnascimento88@gmail.com>
2025-05-09 13:29:28 -0300

    atualizção estrutura

--- projetos_django/firstproject/manage.py
+++ projetos_django/firstproject/manage.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 from pathlib import Path
@@ -9,7 +7,7 @@ sys.path.append(str(ROOT_DIR))
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'firstproject.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'firstproject.settings') # Esta linha já deve estar assim
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
