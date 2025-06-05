d4c386ebed9db0d6674c69cde2e93ca84252dda0
Glailtonnascimento <glailtonnascimento88@gmail.com>
2025-06-05 01:07:18 -0300

    ATUAZEI BANDO DE DADOS

--- PrimeiroProjeto/projectFirst/firstproject/settings.py
+++ PrimeiroProjeto/projectFirst/firstproject/settings.py
@@ -75,7 +75,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django_db',
         'USER': 'root',
-        'PASSWORD': '123',
+        'PASSWORD': '@123',
         'HOST': 'localhost',
         'PORT': '3306',
         'OPTIONS': {
